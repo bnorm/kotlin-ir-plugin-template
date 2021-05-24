fcaad877f5786122aa8e77d857ac3bb0c79e1200
Ryan Nett <JNett96@gmail.com>
2021-05-24 13:44:30 -0700

    Update Kotlin to 1.5.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ buildscript {
 }
 
 plugins {
-  kotlin("jvm") version "1.5.0" apply false
+  kotlin("jvm") version "1.5.10" apply false
   id("org.jetbrains.dokka") version "1.4.32" apply false
   id("com.gradle.plugin-publish") version "0.12.0" apply false
   id("com.github.gmazzo.buildconfig") version "2.1.0" apply false

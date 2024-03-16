c3f940fcc4c015a795b43ea572d3be710ab91810
Todd O'Bryan <toddobryan@gmail.com>
2024-03-15 17:33:55 -0700

    Moved @file:OptIn below copyright

--- kotlin-ir-plugin/src/test/kotlin/com/bnorm/template/IrPluginTest.kt
+++ kotlin-ir-plugin/src/test/kotlin/com/bnorm/template/IrPluginTest.kt
@@ -1,5 +1,3 @@
-@file:OptIn(ExperimentalCompilerApi::class)
-
 /*
  * Copyright (C) 2020 Brian Norman
  *
@@ -15,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+@file:OptIn(ExperimentalCompilerApi::class)
 
 package com.bnorm.template
 
